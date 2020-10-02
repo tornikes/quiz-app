44caa6849fa3f3eb96ffc03dca3b3389a042d51b
Tornike Sadghobelashvili <t.sadghobelashvili@gmail.com>
2020-10-02 10:06:03 +0400

    Updates

---
+++ precache-manifest.edc90dab9c085f5ac0637b74e918f38d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e196b56f8d50a42ae76460267884c76e",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "33f19df99be6b3076eaa",
+    "url": "/quiz-app/static/js/2.e3dcb471.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.e3dcb471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e396ce72118774b7b8",
+    "url": "/quiz-app/static/js/main.d8b8fdfd.chunk.js"
+  },
+  {
+    "revision": "c6ede2a802e4fb20eef6",
+    "url": "/quiz-app/static/js/runtime-main.8027820c.js"
+  }
+]);
